960d888200e2ca2a85605a3a292977ed889b23a8
Abhisek Banerjee <abhisek.banerjee@tu-dortmund.de>
2026-07-16 17:09:40 -0500

    Created recipe for scores and loadings

--- src/WolfRiverPlots.jl
+++ src/WolfRiverPlots.jl
@@ -2,17 +2,36 @@ module WolfRiverPlots
     # dependent packages 
     using DataFrames, Statistics
     using RecipesBase
-    
-    # # utils functions
-    # include("utils.jl");
-    # export sortnatural, pseudoticks,calcKinship2
-    
+
+    # utils functions
+    include("./utils.jl");
+    export get_levels, check_comps
 
     # confidence functions
     include("./confidence/confidence_recipe.jl");
-    export confidenceplot, ConfidencePlot   
+    export confidenceplot, confidenceplot!, ConfidencePlot
 
     include("./confidence/plot_confidence.jl");
     export plot_confidence, plot_confidence!
-    
+
+    # scores functions
+    include("./scores/scores_helpers.jl");
+    export get_scores_coords
+
+    include("./scores/scores_recipe.jl");
+    export scoresplot, scoresplot!, ScoresPlot
+
+    include("./scores/plot_scores.jl");
+    export plot_scores, plot_scores!
+
+    # loadings functions
+    include("./loadings/loadings_helpers.jl");
+    export get_loadings_coords
+
+    include("./loadings/loadings_recipe.jl");
+    export loadingsplot, loadingsplot!, LoadingsPlot
+
+    include("./loadings/plot_loadings.jl");
+    export plot_loadings, plot_loadings!
+
 end # module WolfRiverPlots
